9077a535e34b212b5bfbfa5f26cea5f8633bacef
Amardeep Mann <amar2slick@gmail.com>
2023-01-30 16:40:31 -0800

    Added Power Levels

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,7 @@ let lastFiveNames = [];
 let generateBtn = document.getElementById('generateBtn');
 let clearBtn = document.getElementById('clearBtn');
 let pickTxt = document.getElementById('pickTxt');
+let pwrLvlTxt = document.getElementById('pwrLvlTxt');
 let outputCont = document.getElementById('outputCont');
 
 generateBtn.addEventListener('click', PickName);
@@ -13,7 +14,7 @@ clearBtn.addEventListener('click', ClearList);
 
 function PickName() {
     outputCont.classList.remove('d-none');
-    let pick, randNum;
+    let pick, randNum, pwrLvl;
     let count = 0;
     // This makes it so you never have the same person twice in our last five
     do {
@@ -22,8 +23,17 @@ function PickName() {
         console.log(pick);
         count++;
     } while (lastFiveNames.includes(pick) && count < 50);
+
     pickTxt.textContent = namesList[randNum];
     lastFiveNames.unshift(namesList[randNum]);
+    
+    if (pick.split(' ').length > 1) {
+        pwrLvl = Math.floor(Math.random() * 1000) + 9000;
+    } else {
+        pwrLvl = Math.floor(Math.random() * 5000);
+    }
+    pwrLvlTxt.textContent = pwrLvl;
+
     if (lastFiveNames.length > 5) {
         lastFiveNames = lastFiveNames.slice(0, 5);
     }
